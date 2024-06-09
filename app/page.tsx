16097ea420874c4fa51b5a995a97c3fc27d6f80a
Ardhan Dimas Nuryadin <ardhandimas9@gmail.com>
2024-06-09 23:01:39 +0700

    revisi 3

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import About from "@/components/About";
+import Approuch from "@/components/Approuch";
 import Experience from "@/components/Experience";
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
@@ -15,6 +16,7 @@ export default function Home() {
         <About />
         <Experience />
         <RecentProjects />
+        <Approuch />
         <Footer />
       </div>
     </main>
